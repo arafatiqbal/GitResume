c7a7ab8d32a4aa6d1cbd3779b4f67b401e4f23b5
arafatiqbal <arafatiqbal123@gmail.com>
2023-11-08 16:24:12 -0800

    Navbar and footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+</script>
+
+<footer class="footer footer-center p-4 bg-base-300 text-base-content">
+	<aside>
+		<p>Copyright © 2023 - All right reserved by ACME Industries Ltd</p>
+	</aside>
+</footer>
---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+</script>
+
+<div class="navbar bg-base-100">
+	<a href="/" class="btn btn-ghost normal-case text-xl">GitResume</a>
+</div>
